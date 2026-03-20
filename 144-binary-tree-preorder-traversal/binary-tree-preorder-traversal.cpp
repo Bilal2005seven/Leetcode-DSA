c233389a92bef9068fc86f4aaeb2e03360228c79
Bilal Ahmed <bilal2005official@gmail.com>
2026-03-20 19:45:11 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (8.33%) - LeetSync

---
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+        if (root == NULL) return result;
+
+        result.push_back(root->val);
+
+        vector<int> left = preorderTraversal(root->left);
+        vector<int> right = preorderTraversal(root->right);
+
+        result.insert(result.end(), left.begin(), left.end());
+        result.insert(result.end(), right.begin(), right.end());
+
+        return result;
+    }
+};
