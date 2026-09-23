2d2f4ec8c9f4e5c825e0384650b9f63b6c1f20b4
Bilal Ahmed <bilal2005official@gmail.com>
2026-09-23 21:43:46 +0530

    Time: 53 ms (72.27%) | Memory: 151.1 MB (65.15%) - LeetSync

---
+++ 155-min-stack/min-stack.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class MinStack {
+
+public:
+
+    stack<int> st;
+    stack<int> minst;
+    
+    MinStack() {
+        
+    }
+    
+    void push(int value) {
+
+        st.push(value);
+
+        if (minst.empty()) {
+            minst.push(value);
+        }
+        else {
+            minst.push(min(value, minst.top()));
+        }
+    }
+    
+    void pop() {
+        st.pop();
+        minst.pop();
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return minst.top();
+    }
+};
